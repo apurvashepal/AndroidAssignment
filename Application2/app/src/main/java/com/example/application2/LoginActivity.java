98c951b3fef18c0abe1ef73c364f8979860591e2
Apurva Shepal <apurva.shepal@Eternus.local>
2019-07-31 18:55:30 +0530

    Java Assignmennts

---
+++ Application2/app/src/main/java/com/example/application2/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.application2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
