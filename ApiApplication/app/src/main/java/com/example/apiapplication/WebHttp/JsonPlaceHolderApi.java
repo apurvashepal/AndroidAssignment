f407a19d34555fad6e01fe95e81cf57fe2b2e386
Apurva Shepal <apurva.shepal@Eternus.local>
2019-08-23 19:19:33 +0530

    postmethod

--- ApiApplication/app/src/main/java/com/example/apiapplication/WebHttp/JsonPlaceHolderApi.java
+++ ApiApplication/app/src/main/java/com/example/apiapplication/WebHttp/JsonPlaceHolderApi.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface JsonPlaceHolderApi {
@@ -17,4 +20,10 @@ public interface JsonPlaceHolderApi {
 
     @GET("Posts")
     Call<ResponseBody> getPost(@Query("id") int id);
+
+    @POST("Posts")
+    Call<ResponseBody> createPost(@Field("id") int id,
+                                @Field("userId") int userId,
+                                @Field("title") String title,
+                                @Field("body") String body);
 }
